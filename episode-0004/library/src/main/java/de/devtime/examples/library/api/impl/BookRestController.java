469f2ec2b6900f773e2f78224b5473dea2752083
Morrigan <morrigan@dev-time.de>
2026-01-11 10:47:20 +0100

    feat(spring-boot) episode-0004 Save new books in a database

--- episode-0004/library/src/main/java/de/devtime/examples/library/api/impl/BookRestController.java
+++ episode-0004/library/src/main/java/de/devtime/examples/library/api/impl/BookRestController.java
@@ -1,5 +1,6 @@
 package de.devtime.examples.library.api.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,16 +9,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import de.devtime.examples.library.api.contract.EndpointConstants;
 import de.devtime.examples.library.api.contract.book.BookRegistrationDto;
+import de.devtime.examples.library.persistence.entity.BookEntity;
+import de.devtime.examples.library.persistence.repository.BookRepository;
+import jakarta.transaction.Transactional;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 public class BookRestController {
 
+  @Setter(onMethod_ = @Autowired)
+  private BookRepository bookRepo;
+
   @PostMapping(EndpointConstants.PATH_BOOKS_REGISTRATION)
   @ResponseStatus(HttpStatus.CREATED)
+  @Transactional
   public void registerBook(@RequestBody final BookRegistrationDto registrationDto) {
     log.info("A book registration was requested with the following data: {}", registrationDto);
+
+    BookEntity bookToRegister = BookEntity.builder()
+        .withAuthor(registrationDto.getAuthor())
+        .withIsbn(registrationDto.getIsbn())
+        .withTitle(registrationDto.getTitle())
+        .withIsOnLoan(false)
+        .buildAndInit();
+
+    this.bookRepo.save(bookToRegister);
+
   }
 
 }
